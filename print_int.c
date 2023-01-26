fb9072579328dbf6ea0ff2129f01989a86f408fc
Elizabeth Omae <104896287+liza-emo@users.noreply.github.com>
2023-01-26 23:46:50 +0300

    Updated print int

--- print_int.c
+++ print_int.c
@@ -3,17 +3,18 @@
 /**
  * print_negative - Print a negative multiple digit number
  * @n: Number to print
- * Return: 1 if number is negative, 0 otherwise
+ * Return: The number of characters that are printed
  */
 int print_negative(int n)
 {
-	int max;
-	int zeros;
+	int max, zeros, c;
 
 	zeros = 0;
+	c = 0;
 	if (n < 0)
 	{
 		_putchar('-');
+		c++;
 		for (max = -1000000000; max <= -1; max /= 10)
 		{
 			if (max == -1)
@@ -23,16 +24,20 @@ int print_negative(int n)
 			if (n > max)
 			{
 				if (zeros)
+				{
 					_putchar('0');
+					c++;
+				}
 			}
 			else
 			{
 				zeros = 1;
 				_putchar('0' + (n / max));
+				c++;
 				n %= (-max);
 			}
 		}
-		return (1);
+		return (c);
 	}
 	return (0);
 }
@@ -40,14 +45,15 @@ int print_negative(int n)
 /**
  * print_number - Print a multiple digit number including negative sign
  * @n: Number to print
+ * Return: The number of characters that are printed
  */
-void print_number(int n)
+int print_number(int n)
 {
-	int max;
-	int zeros;
+	int max, zeros, c;
 
 	zeros = 0;
-	if (!(print_negative(n)))
+	c = 0;
+	if (n >= 0)
 	{
 		for (max = 1000000000; max >= 1; max /= 10)
 		{
@@ -58,25 +64,35 @@ void print_number(int n)
 			if (n < max)
 			{
 				if (zeros)
+				{
 					_putchar('0');
+					c++;
+				}
 			}
 			else
 			{
 				zeros = 1;
 				_putchar('0' + (n / max));
 				n %= max;
+				c++;
 			}
 		}
+		return (c);
+	}
+	else
+	{
+		return (print_negative(n));
 	}
 }
 
 /**
- * print_char- Prints a char from the first element of
+ * print_int- Prints an int from the first element of
  *  a given variable list
  * @var: Pointer to the variable list
  * Return: The number of characters that are printed
  */
-int print_char(va_list *var)
+int print_int(va_list *var)
 {
-	return (_putchar(va_arg(*var, int)));
+	print_number(va_arg(*var, int));
+	return (1);
 }
