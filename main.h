57a917d57e4d67460b91c68b9a5443f2fa7ceb66
AlvyneZ <alvynew@gmail.com>
2023-01-26 21:23:02 +0300

    Setting up header file

--- main.h
+++ main.h
@@ -1,10 +1,31 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
-#include <stdio.h>
-#include <stdarg.h>
+#define SUPPORTED_SPEC_COUNT 2
+
 #include <stdlib.h>
+#include <stdarg.h>
+
+typedef int (*print_func_t)(va_list *);
+
+/**
+ * struct format_spec_s - Format specifier details
+ * @spec: Type Specifier character
+ * @print_func: Function to print the given type
+ */
+typedef struct format_spec_s
+{
+	char spec;
+	print_func_t print_func;
+} format_spec_t;
+
+int _putchar(char c);
+
+int _printf(const char *format, ...);
+
+int print_char(va_list *var);
+int print_str(va_list *var);
 
 void my_printf(const char *format, ...);
 
-#endif
+#endif /* _MAIN_H_ */
